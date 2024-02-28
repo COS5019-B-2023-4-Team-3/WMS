1c6f0321808ce3ea85191d5f9577d8f112160a4b
wilfullmurder <mrfarrow2013@gmail.com>
2024-02-28 07:14:51 +0000

    adds OrderItem POJO for mimicking order_items table rows

---
+++ src/main/java/org/Team3/Entities/OrderItem.java
@@ -0,0 +1,93 @@
+package org.Team3.Entities;
+import javax.persistence.*;
+
+/**
+ * The OrderItem class represents an individual item within an order and includes fields for id, order, product, quantity, and price.
+ * It has many-to-one relationships with both Order and Product classes, meaning each order item belongs to one order and one product.
+ */
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "item_cost")
+    private double cost;
+
+    @Column(name = "item_selling_price")
+    private double sellingPrice;
+
+    @Column(name = "order_item_profit")
+    private double profit;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(double sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public double getProfit() {
+        return profit;
+    }
+
+    public void setProfit(double profit) {
+        this.profit = profit;
+    }
+}
+
