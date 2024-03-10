37d18dbec6f010802153f8f7689df3b5fe7d2b83
wilfullmurder <mrfarrow2013@gmail.com>
2024-03-10 08:29:27 +0000

    updates table mapping as it was missed during refactoring and created table for raw_materials

--- src/main/java/org/Team3/Controllers/RawIngredientController.java
+++ src/main/java/org/Team3/Controllers/RawIngredientController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/raw-materials")
+@RequestMapping("/raw-ingredients")
 public class RawIngredientController {
 
     @Autowired
