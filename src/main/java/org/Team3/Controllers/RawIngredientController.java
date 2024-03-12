4b31b0a3f34925a70051d575018df4c8e6ca32f5
mhad15 <m.mahad160203@gmail.com>
2024-03-12 03:07:06 +0000

    redirects to raw ingredient page

--- src/main/java/org/Team3/Controllers/RawIngredientController.java
+++ src/main/java/org/Team3/Controllers/RawIngredientController.java
@@ -16,6 +16,9 @@ public class RawIngredientController {
     @Autowired
     private RawIngredientService rawIngredientService;
 
+    @GetMapping("/raw-ingredients")
+    public String showPage() {return "raw-ingredients";}
+
     @PostMapping
     public ResponseEntity<RawIngredient> createRawMaterial(@RequestBody RawMaterialDto rawMaterialDto) {
         RawIngredient createdRawIngredient = rawIngredientService.createRawMaterial(rawMaterialDto);
