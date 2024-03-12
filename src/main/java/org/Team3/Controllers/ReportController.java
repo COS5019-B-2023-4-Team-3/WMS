06f28d169edf89b8eaade27e91cf4120d51b0c85
nzjsjahs <jameisname@gmail.com>
2024-03-12 04:50:21 +0000

    populates the reports page

--- src/main/java/org/Team3/Controllers/ReportController.java
+++ src/main/java/org/Team3/Controllers/ReportController.java
@@ -1,6 +1,8 @@
 package org.Team3.Controllers;
 
-import org.Team3.Services.ReportService;
+import
+
+        org.Team3.Services.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
