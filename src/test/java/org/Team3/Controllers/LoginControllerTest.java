2c45293a8b31bd99d2f858e8a58631dfd6a9072f
wilfullmurder <mrfarrow2013@gmail.com>
2024-03-12 14:39:48 +0000

    implements WebDriver initializations for ng test suite adds several integration tests for selenium

--- src/test/java/org/Team3/Controllers/LoginControllerTest.java
+++ src/test/java/org/Team3/Controllers/LoginControllerTest.java
@@ -1,13 +1,17 @@
 package org.Team3.Controllers;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -16,90 +20,95 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginControllerTest {
 
-    private WebDriver chromeDriver;
-    private WebDriver fireFoxDriver;
+    public WebDriver driver;
+    public String hubURL = "http://0.0.0.0:4444";
 
-    @BeforeEach
-    public void setUp() throws MalformedURLException {
-        System.out.println("Setting up test: ");
-        // Set up Chrome browser
-        System.out.println("initializing chrome driver: ");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
-
-        // Set up Firefox browser
-        System.out.println("initializing firefox driver: ");
-        FirefoxOptions firefoxOptions = new FirefoxOptions();
-        fireFoxDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd.hub"), firefoxOptions);
+    @Parameters({"browser"})
+    @BeforeTest
+    public void setDriver(String browser) throws MalformedURLException {
+        switch(browser){
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                driver = new RemoteWebDriver(new URL(hubURL), chromeOptions);
+                break;
+            case "edge":
+                EdgeOptions edgeOptions = new EdgeOptions();
+                driver = new RemoteWebDriver(new URL(hubURL), edgeOptions);
+                break;
+            case "firefox":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                driver = new RemoteWebDriver(new URL(hubURL), firefoxOptions);
+                break;
+        }
+        driver.get("http://localhost:8081/");
     }
 
     @Test
-    public void testChromeLoginWithValidCredentials() {
-        System.out.println("Running test: testChromeLoginWithValidCredentials");
-
-        chromeDriver.get("http://localhost:8081/login");
-        // Perform login with valid credentials
-         chromeDriver.findElement(By.id("username")).sendKeys("test_admin");
-         chromeDriver.findElement(By.id("password")).sendKeys("420c23872305988507ab80a1213a23bb43ade60e9773de1d9a062c7f656132c3280a8b3d040ef380");
-         chromeDriver.findElement(By.id("loginButton")).click();
-
-        // Assert that the user is redirected to the homepage
-        System.out.println("chrome: " + chromeDriver.getCurrentUrl());
-        assertEquals("http://localhost:8081/homepage", chromeDriver.getCurrentUrl());
+    public void testGoToLoginPage() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.urlToBe("http://localhost:8081/login"));
+        assertEquals("http://localhost:8081/login", driver.getCurrentUrl(), "User is on the login page");
     }
 
     @Test
-    public void testChromeLoginWithInvalidCredentials() {
-        System.out.println("Running test: testChromeLoginWithInvalidCredentials");
-        chromeDriver.get("http://localhost:8081/login");
+    public void testLoginWithInvalidCredentials() {
+        System.out.println("Running test: testLoginWithInvalidCredentials");
+        driver.get("http://localhost:8081/login");
         // Perform login with invalid credentials
-         chromeDriver.findElement(By.id("username")).sendKeys("invalid_username");
-         chromeDriver.findElement(By.id("password")).sendKeys("invalid_password");
-         chromeDriver.findElement(By.id("loginButton")).click();
+        driver.findElement(By.id("username")).sendKeys("invalid_username");
+        driver.findElement(By.id("password")).sendKeys("invalid_password");
+        driver.findElement(By.id("loginButton")).click();
 
         // Assert that the user stays on the login page and sees the error message
-        System.out.println("chrome: " + chromeDriver.getCurrentUrl());
-        assertEquals("http://localhost:8081/login?error=invalid_username_or_password", chromeDriver.getCurrentUrl());
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+        assertEquals("http://localhost:8081/login?error=invalid_username_or_password", driver.getCurrentUrl());
     }
 
     @Test
-    public void testFirefoxLoginWithValidCredentials() {
-        System.out.println("Running test: testFirefoxLoginWithValidCredentials");
-        fireFoxDriver.get("http://localhost:8081/login");
-        // Perform login with valid credentials
-        fireFoxDriver.findElement(By.id("username")).sendKeys("test_admin");
-        fireFoxDriver.findElement(By.id("password")).sendKeys("420c23872305988507ab80a1213a23bb43ade60e9773de1d9a062c7f656132c3280a8b3d040ef380");
-        fireFoxDriver.findElement(By.id("loginButton")).click();
-
-        // Assert that the user is redirected to the homepage
-        System.out.println("firefox: " + fireFoxDriver.getCurrentUrl());
-        assertEquals("http://localhost:8081/homepage", fireFoxDriver.getCurrentUrl());
-    }
-
-    @Test
-    public void testFirefoxLoginWithInvalidCredentials() {
-        System.out.println("Running test: testFirefoxLoginWithInvalidCredentials");
-        chromeDriver.get("http://localhost:8081/login");
+    public void testLoginWithValidCredentials_Admin() {
+        System.out.println("Running test: testLoginWithValidCredentials");
+        driver.get("http://localhost:8081/login");
         // Perform login with invalid credentials
-        chromeDriver.findElement(By.id("username")).sendKeys("invalid_username");
-        chromeDriver.findElement(By.id("password")).sendKeys("invalid_password");
-        chromeDriver.findElement(By.id("loginButton")).click();
+        driver.findElement(By.id("username")).sendKeys("test_admin");
+        driver.findElement(By.id("password")).sendKeys("test");
+        driver.findElement(By.id("loginButton")).click();
 
         // Assert that the user stays on the login page and sees the error message
-        System.out.println("firefox: " + fireFoxDriver.getCurrentUrl());
-        assertEquals("http://localhost:8081/login?error=invalid_username_or_password", chromeDriver.getCurrentUrl());
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+        assertEquals("http://localhost:8081/homepage", driver.getCurrentUrl());
     }
 
-    @AfterEach
+    @Test
+    public void testLoginWithValidCredentials_Employee() {
+        System.out.println("Running test: testLoginWithValidCredentials");
+        driver.get("http://localhost:8081/login");
+        // Perform login with invalid credentials
+        driver.findElement(By.id("username")).sendKeys("test_employee");
+        driver.findElement(By.id("password")).sendKeys("test");
+        driver.findElement(By.id("loginButton")).click();
+
+        // Assert that the user stays on the login page and sees the error message
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+        assertEquals("http://localhost:8081/homepage", driver.getCurrentUrl());
+    }
+
+    @Test
+    public void testLoginWithValidCredentials_Vendor() {
+        System.out.println("Running test: testLoginWithValidCredentials");
+        driver.get("http://localhost:8081/login");
+        // Perform login with invalid credentials
+        driver.findElement(By.id("username")).sendKeys("test_vendor");
+        driver.findElement(By.id("password")).sendKeys("test");
+        driver.findElement(By.id("loginButton")).click();
+
+        // Assert that the user stays on the login page and sees the error message
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+        assertEquals("http://localhost:8081/homepage", driver.getCurrentUrl());
+    }
+
+    @AfterTest
     public void tearDown() {
         System.out.println("Tearing down tests: ");
-        if(chromeDriver != null) {
-            System.out.println("Quitting chrome driver: ");
-            chromeDriver.quit();
-        }
-        if(fireFoxDriver != null) {
-            System.out.println("Quitting chrome driver: ");
-            fireFoxDriver.quit();
-        }
+        if(driver != null) driver.quit();
     }
 }
