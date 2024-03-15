a0c47659833476a6191540a0e89a281a2dce7388
mhad15 <m.mahad160203@gmail.com>
2024-03-15 03:41:51 +0000

    Formatted page to fit with sidebar.

--- src/main/java/org/Team3/Controllers/RawIngredientController.java
+++ src/main/java/org/Team3/Controllers/RawIngredientController.java
@@ -18,7 +18,12 @@ public class RawIngredientController {
     private RawIngredientService rawIngredientService;
 
     @GetMapping("/raw-ingredients")
-    public String showPage() { return "raw-ingredients"; }}
+    public String showPage() {
+        return "raw-ingredients";
+    }
+
+
+}
 
 //    @PostMapping
 //    public ResponseEntity<RawIngredient> createRawMaterial(@RequestBody RawMaterialDto rawMaterialDto) {
