0fffba6140cdb5be46215d2b305cab802e9faab2
wilfullmurder <mrfarrow2013@gmail.com>
2024-03-22 18:19:08 +0000

    updates testng suite

--- src/test/java/org/Team3/Controllers/RegisterControllerTest.java
+++ src/test/java/org/Team3/Controllers/RegisterControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -91,10 +90,5 @@ public class RegisterControllerTest extends AbstractTestNGSpringContextTests {
         if(user != null){
             userService.deleteUser(user.getId());
         }
-    }
-
-    @AfterTest
-    public void tearDown(){
-
     }
 }
