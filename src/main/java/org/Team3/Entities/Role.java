f354a9d8da26c2bdb592b6d9b216c588181005d7
wilfullmurder <mrfarrow2013@gmail.com>
2024-04-03 23:08:12 +0100

    updates documentation in Entities package classes

--- src/main/java/org/Team3/Entities/Role.java
+++ src/main/java/org/Team3/Entities/Role.java
@@ -1,6 +1,12 @@
 package org.Team3.Entities;
 import javax.persistence.*;
 
+/**
+ * Role class represents a role entity in the application.
+ *
+ * This entity is mapped to the "roles" table in the database.
+ * It includes fields for the role ID and role name.
+ */
 @Entity
 @Table(name = "roles")
 public class Role {
@@ -12,29 +18,62 @@ public class Role {
     @Column(name = "role_name")
     private String name;
 
+    /**
+     * Default constructor for Role class.
+     */
     public Role() {
     }
 
+    /**
+     * Parameterized constructor for Role class.
+     *
+     * @param roleName String representing the name of the role.
+     */
     public Role(String roleName) {
         this.name = roleName;
     }
 
+    /**
+     * Retrieves the ID of the role.
+     *
+     * @return Long representing the ID of the role.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the role.
+     *
+     * @param id Long representing the ID of the role.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the name of the role.
+     *
+     * @return String representing the name of the role.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the role.
+     *
+     * @param name String representing the name of the role.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns a string representation of the Role object.
+     *
+     * @return String representing the Role object.
+     */
     @Override
     public String toString() {
         return "Role{" +
--- src/main/java/org/Team3/Entities/User.java
+++ src/main/java/org/Team3/Entities/User.java
@@ -10,9 +10,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * represent the data model of the warehouse system
- * annotated with JPA annotations to define their mapping to database tables
- * TODO: may want to consider encrypting passwords for security purposes and handling user roles and permissions for access control.
+ * User class represents a user entity in the application's warehouse system.
+ *
+ * This entity is annotated with JPA annotations to define its mapping to the "users" table in the database.
+ * It implements the UserDetails interface provided by Spring Security for user authentication and authorization.
  */
 @Entity
 @Table(name = "users")
@@ -33,57 +34,111 @@ public class User implements UserDetails {
     @JoinColumn(name = "role_id")
     private Role role;
 
+    /**
+     * Default constructor for User class.
+     */
     public User() {
     }
 
-    // Other properties like full name, etc. can be added as needed
-
+    /**
+     * Retrieves the ID of the user.
+     *
+     * @return Long representing the ID of the user.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the user.
+     *
+     * @param id Long representing the ID of the user.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the username of the user.
+     *
+     * @return String representing the username of the user.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Retrieves the password of the user.
+     *
+     * @return String representing the password of the user.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the password of the user.
+     *
+     * @param password String representing the password of the user.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Sets the username of the user.
+     *
+     * @param username String representing the username of the user.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
-
-
+    /**
+     * Determines if the user's account is non-expired.
+     *
+     * @return boolean value indicating if the user's account is non-expired.
+     */
     @Override
     public boolean isAccountNonExpired() {
         return false;
     }
 
+    /**
+     * Determines if the user's account is non-locked.
+     *
+     * @return boolean value indicating if the user's account is non-locked.
+     */
     @Override
     public boolean isAccountNonLocked() {
         return false;
     }
 
+    /**
+     * Determines if the user's credentials are non-expired.
+     *
+     * @return boolean value indicating if the user's credentials are non-expired.
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return false;
     }
 
+    /**
+     * Determines if the user's account is enabled.
+     *
+     * @return boolean value indicating if the user's account is enabled.
+     */
     @Override
     public boolean isEnabled() {
         return false;
     }
 
+    /**
+     * Retrieves the authorities granted to the user.
+     *
+     * @return Collection of GrantedAuthority representing the authorities granted to the user.
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Set<GrantedAuthority> authorities = new HashSet<>();
@@ -91,11 +146,20 @@ public class User implements UserDetails {
         return authorities;
     }
 
-
+    /**
+     * Retrieves the role of the user.
+     *
+     * @return Role object representing the role of the user.
+     */
     public Role getRole() {
         return role;
     }
 
+    /**
+     * Sets the role of the user.
+     *
+     * @param role Role object representing the role of the user.
+     */
     public void setRole(Role role) {
         this.role = role;
     }
