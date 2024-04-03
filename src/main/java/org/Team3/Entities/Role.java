b2314c851b1051ce08527f09981c73435a409f7a
wilfullmurder <mrfarrow2013@gmail.com>
2024-04-03 23:08:12 +0100

    updates documentation in Entities package classes

--- src/main/java/org/Team3/Entities/Role.java
+++ src/main/java/org/Team3/Entities/Role.java
@@ -2,16 +2,14 @@ package org.Team3.Entities;
 import javax.persistence.*;
 
 /**
- * The Role class represents a role in the system, such as "ADMIN", "EMPLOYEE", etc.
+ * Role class represents a role entity in the application.
  *
- * It includes fields for the role's ID and name.
- *
- * The class is annotated with JPA annotations to define its mapping to the database table named "roles".
+ * This entity is mapped to the "roles" table in the database.
+ * It includes fields for the role ID and role name.
  */
 @Entity
 @Table(name = "roles")
 public class Role {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="role_id")
@@ -20,30 +18,53 @@ public class Role {
     @Column(name = "role_name")
     private String name;
 
+    /**
+     * Default constructor for Role class.
+     */
     public Role() {
     }
 
     /**
-     * Constructor for Role class with parameter.
+     * Parameterized constructor for Role class.
      *
-     * @param roleName The name of the role.
+     * @param roleName String representing the name of the role.
      */
     public Role(String roleName) {
         this.name = roleName;
     }
 
+    /**
+     * Retrieves the ID of the role.
+     *
+     * @return Long representing the ID of the role.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the role.
+     *
+     * @param id Long representing the ID of the role.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the name of the role.
+     *
+     * @return String representing the name of the role.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the role.
+     *
+     * @param name String representing the name of the role.
+     */
     public void setName(String name) {
         this.name = name;
     }
@@ -51,7 +72,7 @@ public class Role {
     /**
      * Returns a string representation of the Role object.
      *
-     * @return String containing the ID and name of the role.
+     * @return String representing the Role object.
      */
     @Override
     public String toString() {
