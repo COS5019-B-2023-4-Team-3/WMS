6b5297943a88462470eed9609b94a4d13dc2f6ec
wilfullmurder <mrfarrow2013@gmail.com>
2024-04-03 23:27:55 +0100

    adds test classes for AlertController and ProductController with initial showPage test methods

--- src/test/java/org/Team3/Controllers/ProductControllerTest.java
+++ src/test/java/org/Team3/Controllers/ProductControllerTest.java
@@ -1,48 +1,28 @@
 package org.Team3.Controllers;
 
-import org.Team3.Entities.User;
-import org.Team3.Services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.testng.annotations.BeforeClass;
+import org.Team3.Services.ProductService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
+public class ProductControllerTest {
+    @Mock
+    ProductService productService;
+    @InjectMocks
+    ProductController productController;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-public class ProductControllerTest extends AbstractTestNGSpringContextTests {
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Autowired
-    private UserService userService;
-
-    private MockMvc mockMvc;
-
-    @BeforeClass
+    @BeforeMethod
     public void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    public void testShowPage() throws Exception {
-        mockMvc.perform(get("/products")).andExpect(status().isOk())
-                .andExpect(content().contentType("text/html;charset=UTF-8"));
+    public void testShowPage() {
+        String result = productController.showPage();
+        Assert.assertEquals(result, "products");
     }
+
 }
-//
-//    public class ProductControllerTest {
-//}
