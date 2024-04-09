4c182a257fcae6b8801ef243afa5278e008e0590
nzjsjahs <jameisname@gmail.com>
2024-04-09 15:51:36 +0100

    tests show page for different views

--- src/test/java/org/Team3/Controllers/ReportControllerTest.java
+++ src/test/java/org/Team3/Controllers/ReportControllerTest.java
@@ -1,37 +1,43 @@
 package org.Team3.Controllers;
 
-import org.Team3.Services.UserService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
-public class ReportControllerTest extends AbstractTestNGSpringContextTests {
+@AutoConfigureMockMvc
+public class ReportControllerTest {
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Autowired
-    private UserService userService;
-
     private MockMvc mockMvc;
 
-    @BeforeClass
-    public void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-    }
     @Test
-    public void testShowPage() throws Exception {
-        mockMvc.perform(get("/reports")).andExpect(status().isOk())
-                .andExpect(content().contentType("text/html;charset=UTF-8"));
+    @WithMockUser(username = "test_employee", roles = {"EMPLOYEE"})
+    public void testShowHomepageForAuthorizedEMPLOYEE() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/reports"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("reports"));
+    }
+
+    @Test
+    @WithMockUser(username = "test_admin", roles = {"ADMIN"})
+    public void testShowHomepageForAuthorizedADMIN() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/reports"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("reports"));
+    }
+
+    @Test
+    @WithMockUser(username = "test_vendor", roles = {"EXTERNAL"})
+    public void testShowHomepageForAuthorizedEXTERNAL() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/reports"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("reports"));
     }
 }
