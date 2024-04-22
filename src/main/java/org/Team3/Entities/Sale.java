c0ff78649bd54346e9e41edbbc589f96c948758c
nzjsjahs <jameisname@gmail.com>
2024-04-22 12:33:12 +0100

    create getters and setters in Sale entity

--- src/main/java/org/Team3/Entities/Sale.java
+++ src/main/java/org/Team3/Entities/Sale.java
@@ -14,6 +14,54 @@ public class Sale {
     @Column(name = "transaction_date")
     private LocalDate date;
 
+    @Column(name = "revenue")
+    private Long income;
+
+    @Column(name = "order_volume")
+    private Long volume;
+
+    @Column(name = "order_id")
+    private Long oid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Long getIncome() {
+        return income;
+    }
+
+    public void setIncome(Long income) {
+        this.income = income;
+    }
+
+    public Long getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Long volume) {
+        this.volume = volume;
+    }
+
+    public Long getOid() {
+        return oid;
+    }
+
+    public void setOid(Long oid) {
+        this.oid = oid;
+    }
 
 }
 
