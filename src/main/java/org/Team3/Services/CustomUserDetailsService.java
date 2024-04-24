48e481978c079132f0b2d317d86aca1ca0615da6
WilfullMurder <mrfarrow2013@gmail.com>
2024-04-24 21:54:19 +0100

    Changes ProductService field injection to constructor injection in AlertService.

--- src/main/java/org/Team3/Services/CustomUserDetailsService.java
+++ src/main/java/org/Team3/Services/CustomUserDetailsService.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-
     private final UserRepository userRepository;
 
     public CustomUserDetailsService(UserRepository userRepository) {
