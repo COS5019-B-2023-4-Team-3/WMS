eec671c19b2d979d5dcf7686035bb0b219081cf6
mhad15 <m.mahad160203@gmail.com>
2024-04-26 23:58:54 +0100

    added description to raw ingredient class

--- src/main/java/org/Team3/Entities/RawIngredient.java
+++ src/main/java/org/Team3/Entities/RawIngredient.java
@@ -31,9 +31,10 @@ public class RawIngredient {
      * @param name     The name of the raw ingredient.
      * @param quantity The quantity of the raw ingredient.
      */
-    public RawIngredient(String name, int quantity) {
+    public RawIngredient(String name, int quantity, String description) {
         this.name = name;
         this.quantity = quantity;
+        this.description = description;
     }
 
     public Long getId() {
