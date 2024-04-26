0ec88f91d9b74f9e05001c921e64748cfdfd1aef
WilfullMurder <mrfarrow2013@gmail.com>
2024-04-26 20:08:09 +0100

    updates Apexchart in reports view for dynamic chart generation.
    refactors showPage method to utilize new
    SaleService lastWeekSales method
    
    adds new implementation to SaleService
    .getLastWeekSales method

--- src/main/java/org/Team3/Controllers/ReportController.java
+++ src/main/java/org/Team3/Controllers/ReportController.java
@@ -3,18 +3,13 @@ package org.Team3.Controllers;
 import org.Team3.Entities.Sale;
 import org.Team3.Services.ReportService;
 import org.Team3.Services.SaleService;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.entity.JFreeChartEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * The ReportController class contains endpoints for generating reports.
@@ -22,6 +17,7 @@ import java.util.List;
 @Controller
 
 public class ReportController {
+
     @Autowired
     private ReportService reportService;
 
@@ -35,29 +31,14 @@ public class ReportController {
      */
     @GetMapping("/reports")
     public String showPage(Model model) {
-        //get specified range of sales
-        //add as a model attribute
-        //use model to access data in html
-        List<Sale> salesInRange = saleService.getWeekSales();
-        Model salesData = model.addAttribute("weekSaleData", salesInRange);
+        List<Map<String, Object>> salesInRange = saleService.getLastWeekSales();
+
+        System.out.println(salesInRange);
+
+        model.addAttribute("salesData", salesInRange);
         return "reports";
     }
 
 
 }
-/**
-     * Endpoint for generating the product inventory report.
-     * @return ResponseEntity with a success message and HTTP status OK if the report is generated successfully,
-     *         or an error message and HTTP status INTERNAL_SERVER_ERROR if there's an error during report generation
-     */
-//    @GetMapping("/product-inventory")
-//    public ResponseEntity<String> generateProductInventoryReport() {
-//        try {
-//            reportService.generateProductInventoryReport("product-inventory.xlsx");
-//            return new ResponseEntity<>("Product inventory report generated successfully", HttpStatus.OK);
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//            return new ResponseEntity<>("Failed to generate product inventory report", HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
 
