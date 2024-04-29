3bf89662e402ee5d0cd67fcacd73c9de4ea6fda8
nzjsjahs <jameisname@gmail.com>
2024-04-29 20:16:38 +0100

    adds data to table from sql data adds line chart to pdf report

--- src/main/java/org/Team3/Services/PDFGeneratorService.java
+++ src/main/java/org/Team3/Services/PDFGeneratorService.java
@@ -1,33 +1,44 @@
 package org.Team3.Services;
 
-import com.lowagie.text.*;
-import com.lowagie.text.Font;
-import com.lowagie.text.pdf.PdfPCell;
-import com.lowagie.text.pdf.PdfPTable;
-import com.lowagie.text.pdf.PdfWriter;
+import com.itextpdf.text.*;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Image;
+
 import org.Team3.Entities.Sale;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class PDFGeneratorService {
-    private final List<Sale> getAllSales;
-
-    public PDFGeneratorService(List<Sale> getAllSales) {
-        this.getAllSales = getAllSales;
-    }
-
+@Autowired SaleService saleService;
     private void tableHeader(PdfPTable table) {
         PdfPCell cell = new PdfPCell();
-        cell.setBackgroundColor(Color.WHITE);
+        cell.setBackgroundColor(BaseColor.WHITE);
         cell.setPadding(5);
 
         Font font = FontFactory.getFont(FontFactory.HELVETICA);
-        font.setColor(Color.BLACK);
+        font.setColor(BaseColor.BLACK);
 
         cell.setPhrase(new Phrase("Sale ID", font));
         table.addCell(cell);
@@ -43,11 +54,10 @@ public class PDFGeneratorService {
 
         cell.setPhrase(new Phrase("Order ID", font));
         table.addCell(cell);
-
     }
 
     private void tableData(PdfPTable table) {
-        for (Sale sale : getAllSales) {
+        for (Sale sale : saleService.getAllSales()) {
             table.addCell(String.valueOf(sale.getId()));
             table.addCell(sale.getDate().toString());
             table.addCell(sale.getIncome().toString());
@@ -55,24 +65,34 @@ public class PDFGeneratorService {
             table.addCell(String.valueOf(sale.getOrderId()));
         }
     }
-    public void export(HttpServletResponse response) throws IOException {
+    public void export(HttpServletResponse response, List<Map<String, Object>> data) throws IOException, DocumentException {
         Document document = new Document(PageSize.A4);
         PdfWriter.getInstance(document, response.getOutputStream());
 
         document.open();
+
         Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
         fontTitle.setSize(18);
 
-        Paragraph paragraph = new Paragraph("Sales Data", fontTitle);
-        paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+        DateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
+        String currentDateTime = dateFormatter.format(date);
+
+        //Title
+        Paragraph title = new Paragraph("Sales Report : " + currentDateTime, fontTitle);
+        title.setAlignment(Paragraph.ALIGN_CENTER);
+        document.add(title);
+
+        //Create Line Chart
+        DefaultCategoryDataset dataset = createDataset(data);
+        Image chart = createLineChartFrom(dataset);
+        document.add(chart);
 
         Font fontParagraph = FontFactory.getFont(FontFactory.HELVETICA);
         fontParagraph.setSize(12);
 
         Paragraph paragraph1 = new Paragraph("List of Sales", fontParagraph);
         paragraph1.setAlignment(Paragraph.ALIGN_LEFT);
-
-        document.add(paragraph);
         document.add(paragraph1);
 
         PdfPTable table = new PdfPTable(5);
@@ -87,4 +107,26 @@ public class PDFGeneratorService {
 
         document.close();
     }
+
+    private DefaultCategoryDataset createDataset(List<Map<String, Object>> data) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for (Map<String, Object> entry : data) {
+            dataset.addValue((Number) entry.get("income"), "Income", (String) entry.get("date"));
+        }
+        return dataset;
+    }
+
+    private Image createLineChartFrom(DefaultCategoryDataset dataset) throws BadElementException, IOException {
+        //Create Line Chart
+        JFreeChart lineChart = ChartFactory.createLineChart("Sales", "Data", "Income", dataset);
+
+        //Render chart as image
+        BufferedImage bufferedImage = lineChart.createBufferedImage(500, 400);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
+
+        //Add image to PDF
+        return Image.getInstance(byteArrayOutputStream.toByteArray());
+    }
 }
+
